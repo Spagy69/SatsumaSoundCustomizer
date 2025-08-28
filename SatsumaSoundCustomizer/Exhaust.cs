27cac82ca8a3bfaa2f935977eec8b97ec5925fd7
Spagy <lagy.spagy@gmail.com>
2025-08-28 23:49:33 +0200

    added references to exhaust parts

--- SatsumaSoundCustomizer/Exhaust.cs
+++ SatsumaSoundCustomizer/Exhaust.cs
@@ -15,9 +15,12 @@ namespace SatsumaSoundCustomizer
 
         public static void ExhaustSoundChange()
         {
-            GameObject exhaustPipe = GameObject.Find("SATSUMA(557kg, 248)/MiscParts/pivot_exhaust pipe/exhaust pipe(Clone)");
-            GameObject exhaustMuffler = GameObject.Find("SATSUMA(557kg, 248)/MiscParts/pivot_exhaust_muffler/exhaust muffler(Clone)");
-
+            GameObject headers = GameObject.Find("SATSUMA(557kg, 248)/Chassis/sub frame(xxxxx)/CarMotorPivot/block(Clone)/pivot_cylinder head/cylinder head(Clone)/pivot_headers/headers(Clone)");
+            GameObject stockPipe = GameObject.Find("SATSUMA(557kg, 248)/MiscParts/pivot_exhaust pipe/exhaust pipe(Clone)");
+            GameObject stockMuffler = GameObject.Find("SATSUMA(557kg, 248)/MiscParts/pivot_exhaust_muffler/exhaust muffler(Clone)");
+            GameObject steelHeaders = GameObject.Find("SATSUMA(557kg, 248)/Chassis/sub frame(xxxxx)/CarMotorPivot/block(Clone)/pivot_cylinder head/cylinder head(Clone)/pivot_headers/steel headers(Clone)");
+            GameObject racingPipe = GameObject.Find("SATSUMA(557kg, 248)/MiscParts/pivot_exhaust pipe/racing exhaust(Clone)");
+            GameObject racingMuffler = GameObject.Find("SATSUMA(557kg, 248)/MiscParts/pivot_exhaust_muffler/racing muffler(Clone)");
         }
 
     }
